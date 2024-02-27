179e157659d36e9621942da76c69f683ba1d9e4a
ArjayBuen <arjaybuen10@gmail.com>
2024-02-27 21:14:36 +0800

    learned react props

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function Card(props){
+  return(
+  <div>
+    <h2>{props.name}</h2>
+    <img
+      src={props.img}
+      alt="avatar_img"
+    />
+    <p>{props.tel}</p>
+    <p>{props.email}</p>
+  </div>
+  );
+  
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+    <h1>My Contacts</h1>
+    <Card name="Beyonce" tel="+123 456 789" email="b@beyonce.com"
+    img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+    />
+    <Card name="Jack Bauer" tel="+987 654 321" email="jack@nowhere.com"
+    img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" 
+    />
+    <Card name="Chuck Norris" tel="+918 372 574" email="gmail@chucknorris.com"
+    img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+    />
+  </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
